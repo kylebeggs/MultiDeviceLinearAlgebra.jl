4b492956da0edd0bc786a809d7cb0334a7c9edf3
Documenter.jl <documenter@juliadocs.github.io>
2026-02-26 14:48:27 +0000

    build based on dc35ea0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MultiDeviceLinearAlgebra","page":"Home","title":"MultiDeviceLinearAlgebra","text":"Documentation for MultiDeviceLinearAlgebra.\n\n","category":"section"}]
+}
